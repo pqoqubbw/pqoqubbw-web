c7a89977c0630b047d83b14bc4b3ace1cde6458f
pqoqubbw <pqoqubbw@gmail.com>
2024-10-20 00:41:06 +0200

    wip

--- components/providers/index.tsx
+++ components/providers/index.tsx
@@ -2,10 +2,15 @@ import { AppThemeProvider } from "@/components/theme";
 
 import { ViewTransitions } from "next-view-transitions";
 
+import { Openpanel } from "../analytics/openpanel";
+
 export const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <ViewTransitions>
-      <AppThemeProvider>{children}</AppThemeProvider>
+      <AppThemeProvider>
+        {children}
+        <Openpanel />
+      </AppThemeProvider>
     </ViewTransitions>
   );
 };
