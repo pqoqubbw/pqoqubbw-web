6a27fb7f8bf82dca4ed3017cc81e1d3599062af8
pqoqubbw <pqoqubbw@gmail.com>
2024-10-29 20:56:20 +0100

    add more icons

--- examples/micro-interactions/index.tsx
+++ examples/micro-interactions/index.tsx
@@ -1,14 +1,21 @@
+import { ArchiveIcon } from "./icons/archive";
+import { BadgePercentIcon } from "./icons/badge-percent";
 import { BellIcon } from "./icons/bell";
+import { CalendarCogIcon } from "./icons/calendar-cog";
 import { ChartPieIcon } from "./icons/chart-pie";
+import { CircleDollarSignIcon } from "./icons/circle-dollar-sign";
 import { CursorClickIcon } from "./icons/cursor-click";
 import { DeleteIcon } from "./icons/delete";
 import { DownvoteIcon } from "./icons/downvote";
 import { EditIcon } from "./icons/edit";
 import { ExpandIcon } from "./icons/expand";
+import { FilePenLineIcon } from "./icons/file-pen-line";
 import { FileStackIcon } from "./icons/file-stack";
 import { FingerprintIcon } from "./icons/fingerprint";
 import { GitPullRequestIcon } from "./icons/git-pull-request";
 import { GridIcon } from "./icons/grid";
+import { HandCoinsIcon } from "./icons/hand-coins";
+import { HomeIcon } from "./icons/home";
 import { PartyPopperIcon } from "./icons/party-popper";
 import { RefreshIcon } from "./icons/refresh";
 import { RouteIcon } from "./icons/route";
@@ -18,6 +25,7 @@ import { SettingsGearIcon } from "./icons/settings-gear";
 import { SunIcon } from "./icons/sun";
 import { UnplugIcon } from "./icons/unplug";
 import { UpvoteIcon } from "./icons/upvote";
+import { UsersIcon } from "./icons/users";
 import { VolumeIcon } from "./icons/volume";
 
 const MicroInteractions = () => {
@@ -49,6 +57,17 @@ const MicroInteractions = () => {
         <FingerprintIcon />
         <RouteIcon />
         <VolumeIcon />
+      </div>
+      <div className="flex sm:gap-4 gap-2 items-center flex-wrap">
+        <HomeIcon />
+        <FilePenLineIcon />
+        <ArchiveIcon />
+        <BadgePercentIcon />
+        <HandCoinsIcon />
+        <CalendarCogIcon />
+        <UsersIcon />
+        <ChartPieIcon />
+        <CircleDollarSignIcon />
       </div>
     </div>
   );
