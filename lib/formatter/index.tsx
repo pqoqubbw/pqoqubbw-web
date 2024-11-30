803efc3d60babf86829db504ea8b683095b76aff
pqoqubbw <pqoqubbw@gmail.com>
2024-11-30 21:08:37 +0100

    cleanup

--- lib/formatter/index.tsx
+++ lib/formatter/index.tsx
@@ -6,20 +6,6 @@ const date = (input: Date): string => {
   }).format(input);
 };
 
-const number = (input: number): string => {
-  return new Intl.NumberFormat("en", {
-    style: "decimal",
-  }).format(input);
-};
-
-const time = (value: number, unit: Intl.RelativeTimeFormatUnit): string => {
-  return new Intl.RelativeTimeFormat("en", {
-    numeric: "auto",
-  }).format(value, unit);
-};
-
 export const formatter = {
   date,
-  number,
-  time,
 };
