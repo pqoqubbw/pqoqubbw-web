0c33e0dca97e995692bfe52900a405ed48d2bb0b
pqoqubbw <pqoqubbw@gmail.com>
2024-12-01 03:06:56 +0100

    add snowfall

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "@/styles/main.css";
 import type { Metadata } from "next";
 
 import { Providers } from "@/components/providers";
+import { SnowfallComponent } from "@/components/snowfall";
 import { OpenGraph } from "@/lib/og";
 
 import clsx from "clsx";
@@ -25,8 +26,11 @@ export default function RootLayout({
     >
       <body>
         <Providers>
-          <main className="mx-auto max-w-screen-sm overflow-x-hidden px-6 py-24 md:overflow-x-visible ">
-            <article className="article">{children}</article>
+          <main className="mx-auto max-w-screen-sm overflow-x-hidden px-6 py-24 md:overflow-x-visible">
+            <article className="article">
+              {children}
+              <SnowfallComponent />
+            </article>
           </main>
         </Providers>
       </body>
